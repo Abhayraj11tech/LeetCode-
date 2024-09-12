62f4e4c7d2692d1cfcd40254deab5ef7014d647d
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-09-12 15:37:32 +0530

    Time: 14 ms (54.32%), Space: 54.3 MB (21.08%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int f = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.isEmpty() || nums[i] < nums[stack.peek()]) {
+                stack.push(i);
+            }
+        }
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums[j] >= nums[stack.peek()]) {
+                f = Math.max(f, j - stack.pop());
+            }
+        }
+
+        return f;
+    }
+}
