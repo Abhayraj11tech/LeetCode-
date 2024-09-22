833070d2ff3fd9f3e983631dadab23cfdc57fb78
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-09-22 23:06:11 +0530

    Time: 1 ms (97.65%), Space: 40 MB (99.74%) - LeetHub

---
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distributeCandies(int n, int limit) {
+        
+        int res = 0;
+
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= limit; j++) {
+                if (i + j <= n && i + j >= n - limit) {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
