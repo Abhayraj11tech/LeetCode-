8cb784855a94e3237063ef52c56eca5bde9d6633
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-09-26 18:56:01 +0530

    Time: 33 ms (39.47%), Space: 50.6 MB (79.02%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(
+                (b[0] * b[0] + b[1] * b[1]), 
+                (a[0] * a[0] + a[1] * a[1])
+            )
+        );
+
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int[][] sol = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            sol[i] = maxHeap.poll();
+        }
+
+        return sol;
+    }
+}
