df9353147385416ba49695bb970a181f01d7be04
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-10-08 20:12:34 +0530

    Time: 6 ms (81.37%), Space: 91.6 MB (5.29%) - LeetHub

--- 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
@@ -3,7 +3,7 @@ class Solution {
         int[] rowdiff = new int[grid.length];
         int[] coldiff = new int[grid[0].length];
 
-        //calculate the diff arrays
+     
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] == 1){
