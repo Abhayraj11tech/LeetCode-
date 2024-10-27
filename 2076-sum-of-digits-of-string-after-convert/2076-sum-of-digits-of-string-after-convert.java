b564f4414a985c19866a30b60a7014991c6f8945
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-10-28 00:14:01 +0530

    Time: 2 ms (53.06%), Space: 42 MB (33.38%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getLucky(String s, int k) {
+         StringBuilder sol = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int val =  (int)ch - 96;
+            sol.append(val);
+        }
+        
+        String result = sol.toString();
+       for (int j = 0; j < k; j++) {
+            int sum = 0;
+            for (char ch : result.toCharArray()) {
+                sum += Character.getNumericValue(ch);
+            }
+            result = Integer.toString(sum);
+        }
+
+        return Integer.parseInt(result);
+    }
+}
