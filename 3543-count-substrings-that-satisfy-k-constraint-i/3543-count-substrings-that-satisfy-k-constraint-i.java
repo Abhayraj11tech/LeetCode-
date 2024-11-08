2302e80441045e3d908e89d9a5d053e624ec235e
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-11-08 22:09:46 +0530

    Time: 3 ms (45.73%), Space: 42.4 MB (21.75%) - LeetHub

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countKConstraintSubstrings(String s, int k) {
+        int sol = 0;
+       
+        for (int i = 0; i < s.length(); i++) {
+            int z = 0, o = 0;
+            
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(j) == '0') {
+                    z++;
+                } else {
+                    o++;
+                }
+       
+                if (z <= k || o <= k) {
+                    sol++;
+                }
+            }
+        }
+        
+        return sol;
+    }
+}
