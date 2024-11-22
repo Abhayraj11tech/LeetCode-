80b45e17dc28813e5d148bd41497a72c5667ceda
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-11-22 23:15:41 +0530

    Time: 2 ms (28.57%), Space: 40.3 MB (57.1%) - LeetHub

---
+++ 2679-count-distinct-numbers-on-board/2679-count-distinct-numbers-on-board.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int distinctIntegers(int n) {
+         Set<Integer> set = new HashSet<>();
+        while( n>0)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                if(n%i==1)
+                {
+                    set.add(i);
+                }
+            }
+            n--;
+        }
+        return set.size()+1;
+    }
+}
