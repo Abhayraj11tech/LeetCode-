405079ffaeccdc6873b6c1c7f758e7d0e45f77ce
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-12-25 16:29:57 +0530

    Time: 3 ms (6.1%), Space: 41.9 MB (7.28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,44 +1,27 @@
-import java.lang.Math;
 class Solution {
     public int reverse(int x) {
-
-
-         int y=0;
-        if(x>0){
-            y = x;
-        }
-        else{
-            y = -(x);
-        }
-       
-        int rev = 0;
-        int rem = 0;
-        int sol=0;
-        
-        while(y>0){
-            rem = y%10;
-            rev=rev*10+rem;
-            y=y/10;
-
-        }
-        
-        if(x>0){
-            sol = rev;
-        }
-        else{
-            sol = -(rev);
-        }
-int val=0;
-        if(x<Integer.MIN_VALUE || x>Integer.MAX_VALUE || x>=1147483648 || x==-1563847412 || x==1137464807){
-            val = sol;
-            sol = 0;
-        }
-        if(x==1463847412){
-            sol = val;
+        boolean flag = false;
+        if (x < 0) {
+            flag = true;
         }
 
-        
-        return sol;
+        String str = "";
+        int val = Math.abs(x); 
+        while (val != 0) {
+            str += Integer.toString(val % 10);
+            val = val / 10;
+        }
 
+        String sol = "";
+        if (flag) {
+            sol += "-";
+        }
+
+        sol += str;
+        try {
+            return Integer.parseInt(sol);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 }
