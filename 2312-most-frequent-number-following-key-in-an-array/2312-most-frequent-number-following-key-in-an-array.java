9b3056d94b6f374a0dded7e7f3be9fc27acd5cff
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-01-06 02:23:27 +0530

    Time: 3 ms (77.78%), Space: 44.3 MB (56.39%) - LeetHub

--- 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.java
+++ 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.java
@@ -1,58 +1,23 @@
 class Solution {
     public int mostFrequent(int[] nums, int key) {
-        ArrayList<Integer> elm = new ArrayList<Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==key){
-                if(i+1<nums.length){
-                    int tar = nums[i+1];
-                    elm.add(tar);
-                }
-         }
-
-            
+        Map<Integer, Integer> count = new HashMap<>();
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == key) {
+                int target = nums[i + 1];
+                count.put(target, count.getOrDefault(target, 0) + 1);
+            }
         }
-        Collections.sort(elm);
-        int count = 1;
+        
         int sol = -1;
         int max = Integer.MIN_VALUE;
-        for(int i=0;i<elm.size();i++){
-            if(i+1<elm.size()){
-                if(elm.get(i+1)==elm.get(i)){
-                    ++count;
-                }
-                else{
-                    if(count>max){
-                        sol = elm.get(i);
-                        max = count;
-                    }
-                    count = 1;
-                }
-            }
-            else{
-                ++count;
-                if(count>max){
-                        sol = elm.get(i);
-                        max = count;
-                    }
-                    count = 1;
-
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                sol = entry.getKey();
             }
         }
-        if(sol==997 && key==999){
-            sol = 999;
-        }
-        else if(sol==999 && key==997){
-            sol = 997;
-        }
-        else if(sol==1000 && key==998){
-            sol = 998;
-        }
-        else if(sol==1000 && key==500){
-            sol = 500;
-        }
-        else if(sol==1000 && key==600){
-            sol = 600;
-        }
+        
         return sol;
     }
 }
