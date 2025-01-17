f405bbbb5af52654b95e527ac0fd9c9d37f407a6
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-01-17 23:06:58 +0530

    Time: 935 ms (5.02%), Space: 285.3 MB (5.15%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,29 @@
 class Solution {
     public int countSubstrings(String s) {
-        int count = 0;
-        for(int i = 0; i < s.length(); i++) {
-            String str = "";
+        
+        ArrayList<String> substrings = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+               String str = "";
             for(int j = i; j < s.length(); j++) {
                 str += s.charAt(j);  
-                if(isPalindrome(str)) {
-                    ++count;
-                }
+                substrings.add(str);
             }
         }
+
+        int count = 0;
+        for (String str : substrings) {
+            if (isPalindrome(str)) {
+                count++;
+            }
+        }
+
         return count;
     }
 
     private boolean isPalindrome(String s) {
         int left = 0, right = s.length() - 1;
-        while(left < right) {
-            if(s.charAt(left) != s.charAt(right)) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
             left++;
