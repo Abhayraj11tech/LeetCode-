f1fbf5f59d3f759b06713c78dc245c2712bc2474
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-02-10 20:42:08 +0530

    Time: 316 ms (5.07%), Space: 49.4 MB (5.37%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,39 +1,41 @@
 class MinStack {
-    LinkedList<TplusMin> stack;
-    private class TplusMin {
-        int val;
-        int min;
-        public TplusMin(int val, int min) {
-            this.val = val;
-            this.min = min;
-        }
-    }
+
+    private ArrayList<Integer> arr;
 
     public MinStack() {
-        stack = new LinkedList<>();
+        arr = new ArrayList<>();
     }
     
     public void push(int val) {
-        int newMin;
-        if (stack.size() == 0){
-            newMin = val;
-        }
-        else {
-            int currentMin = stack.getFirst().min;
-            newMin = val < currentMin ? val : currentMin;
-        }
-        stack.addFirst(new TplusMin(val, newMin));
+        arr.add(val);
     }
     
     public void pop() {
-        stack.removeFirst();
+        if(arr.size()==0){
+            return;
+        }
+         arr.remove(arr.size()-1);
     }
     
     public int top() {
-        return stack.peekFirst().val;
+         return arr.get(arr.size()-1);
     }
     
     public int getMin() {
-        return stack.peekFirst().min;
+         if (arr.isEmpty()) {
+            return -1;
+         }
+        ArrayList<Integer> val = new ArrayList<>(arr);
+        Collections.sort(val);
+        return val.get(0);
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
