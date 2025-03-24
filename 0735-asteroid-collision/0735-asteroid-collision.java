c2347c052209910e1caa83b5579fd9596412544f
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-03-25 00:11:44 +0530

    Time: 5 ms (88.01%), Space: 45.1 MB (93.95%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,16 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int[] asteroidCollision(int[] ast) {
         ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < ast.length; i++) {
             int val = ast[i];
             while (!arr.isEmpty() && val < 0 && arr.get(arr.size() - 1) > 0) {
-                int top = arr.remove(arr.size() - 1);
-                if (top == -val) {
+                int last = arr.remove(arr.size() - 1);
+                if (last == -val) {
                     val = 0;
-                } else if (top > -val) {
-                    val = top;
+                } else if (last > -val) {
+                    val = last; 
                 }
             }
             if (val != 0) {
