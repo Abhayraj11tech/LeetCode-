3d39e965ba5d496cd456162fbcbb40919cacc203
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-03-25 19:31:32 +0530

    Time: 1 ms (71.92%), Space: 41.8 MB (41.05%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,37 +1,30 @@
-import java.util.ArrayList;
-
 class Solution {
     public String decodeString(String s) {
-        ArrayList<Integer> num = new ArrayList<>();
-        ArrayList<String> str = new ArrayList<>();
-        StringBuilder currentString = new StringBuilder();
-        int currentNum = 0;
-        int i = 0;
+        Stack<Integer> st = new Stack<>();
+        Stack<StringBuilder> st1 = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int n = 0;
 
-        while (i < s.length()) {
-            char c = s.charAt(i);
-
+        for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
-                currentNum = currentNum * 10 + (c - '0');
+                n = n * 10 + (c - '0');
             } else if (c == '[') {
-                num.add(currentNum);
-                str.add(currentString.toString());
-                currentString = new StringBuilder();
-                currentNum = 0;
+                st.push(n);
+                n = 0;
+                st1.push(sb);
+                sb = new StringBuilder();
             } else if (c == ']') {
-                StringBuilder temp = new StringBuilder(str.remove(str.size() - 1));
-                int repeatCount = num.remove(num.size() - 1);
-                for (int j = 0; j < repeatCount; j++) {
-                    temp.append(currentString);
+                int k = st.pop();
+                StringBuilder temp = sb;
+                sb = st1.pop();
+                while (k-- > 0) {
+                    sb.append(temp);
                 }
-                currentString = temp;
             } else {
-                currentString.append(c);
+                sb.append(c);
             }
-
-            i++;
         }
 
-        return currentString.toString();
+        return sb.toString();
     }
 }
