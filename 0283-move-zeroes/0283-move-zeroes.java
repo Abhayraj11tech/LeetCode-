07b02a8750fedcf0d784a87c69c49bd15e0de5ec
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-04-24 21:40:48 +0530

    Time: 2 ms (84.23%), Space: 46.1 MB (47.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p = 0;
+               int p = 0;
         int copy[] = Arrays.copyOf(nums,nums.length);
    for(int i=0;i<nums.length;i++){
        if(copy[i]!=0){
