eaf63e113cbcff1ab039909dbd8d4f9cb9c04ca3
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-06-25 20:04:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (58.56%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,60 +1,33 @@
 import java.util.*;
 
-// Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class Info {
-    TreeNode node;
-    int level;
-    Info(TreeNode node, int level) {
-        this.node = node;
-        this.level = level;
-    }
-}
-
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Map<Integer, List<Integer>> map = new TreeMap<>();
-        Queue<Info> q = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
 
-        if (root != null) {
-            q.add(new Info(root, 0));
-        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean leftToRight = true;
 
         while (!q.isEmpty()) {
-            Info curr = q.poll();
-            TreeNode node = curr.node;
-            int level = curr.level;
+            int size = q.size();
+            List<Integer> level = new ArrayList<>(size);
 
-            map.putIfAbsent(level, new ArrayList<>());
-            map.get(level).add(node.val);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                
+                if (leftToRight) {
+                    level.add(node.val);
+                } else {
+                    level.add(0, node.val);
+                }
 
-            if (node.left != null) {
-                q.add(new Info(node.left, level + 1));
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
             }
-            if (node.right != null) {
-                q.add(new Info(node.right, level + 1));
-            }
-        }
 
-        List<List<Integer>> res = new ArrayList<>();
-        for (int level : map.keySet()) {
-            List<Integer> list = map.get(level);
-            if (level % 2 == 1) {
-                Collections.reverse(list);
-            }
-            res.add(list);
+            res.add(level);
+            leftToRight = !leftToRight; 
         }
 
         return res;
