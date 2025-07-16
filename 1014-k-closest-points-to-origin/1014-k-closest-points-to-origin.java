edaa666a0b2a80316a3de188d9804ce8c9c70d0a
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2025-07-16 16:26:09 +0530

    Time: 21 ms (91.53%), Space: 53.9 MB (36.83%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,26 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-            (a, b) -> Integer.compare(
-                (b[0] * b[0] + b[1] * b[1]), 
-                (a[0] * a[0] + a[1] * a[1])
-            )
-        );
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            int d = x * x + y * y;
 
-        for (int[] point : points) {
-            maxHeap.offer(point);
-            if (maxHeap.size() > k) {
-                maxHeap.poll();
-            }
+            pq.add(new Info(x, y, d));
         }
 
         int[][] sol = new int[k][2];
         for (int i = 0; i < k; i++) {
-            sol[i] = maxHeap.poll();
+            Info info = pq.poll();
+            sol[i][0] = info.a;
+            sol[i][1] = info.b;
         }
 
         return sol;
+    }
+
+    static class Info implements Comparable<Info> {
+        int a, b, d;
+
+        Info(int a, int b, int d) {
+            this.a = a;
+            this.b = b;
+            this.d = d;
+        }
+
+        public int compareTo(Info i) {
+            return this.d - i.d;
+        }
     }
 }
